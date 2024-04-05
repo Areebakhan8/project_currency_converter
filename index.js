eb4afc1ca6fe38adaffd380b66ab24a3be3659b4
Areebakhan8 <160344226+Areebakhan8@users.noreply.github.com>
2024-04-06 01:34:46 +0500

    currency convertor

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //base curreny
+    EUR: 0.922,
+    AFN: 71.225,
+    AMD: 387.97,
+    LBP: 89.5,
+    CNY: 7.233,
+    INR: 83.273,
+    PKR: 277.73,
+};
+let userInput = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "EUR", "AFN", "AMD", "LBP", "CNY", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "EUR", "AFN", "AMD", "LBP", "CNY", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number"
+    }
+]);
+let fromCurrency = currency[userInput.from];
+let toCurrency = currency[userInput.to];
+let amount = userInput.amount;
+let baseAmount = amount / fromCurrency; //usd base currency
+let convertedAmount = baseAmount * toCurrency;
+console.log(convertedAmount);
