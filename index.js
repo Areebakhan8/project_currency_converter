2136912e5a65c62541e34244dcc186de4ecca5d9
Areebakhan8 <160344226+Areebakhan8@users.noreply.github.com>
2024-04-15 01:39:53 +0500

    currency converter

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-#! usr/bin/env node
-
+#! /usr/bin/env node
 import inquirer from "inquirer";
-
 const currency = {
-  USD: 1, //base curreny
-  EUR: 0.922,
-  AFN: 71.225,
-  AMD: 387.97,
-  LBP: 89.5,
-  CNY: 7.233,
-  INR: 83.273,
-  PKR: 277.73,
+    USD: 1, //base curreny
+    EUR: 0.922,
+    AFN: 71.225,
+    AMD: 387.97,
+    LBP: 89.5,
+    CNY: 7.233,
+    INR: 83.273,
+    PKR: 277.73,
 };
 let userInput = await inquirer.prompt([
-  {
-    name: "from",
-    message: "Enter From Currency",
-    type: "list",
-    choices: ["USD", "EUR", "AFN", "AMD", "LBP", "CNY", "INR", "PKR"],
-  },
-  {
-    name: "to",
-    message: "Enter To Currency",
-    type: "list",
-    choices: ["USD", "EUR", "AFN", "AMD", "LBP", "CNY", "INR", "PKR"],
-  },
-  {
-    name: "amount",
-    message: "Enter Your Amount",
-    type: "number",
-  },
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "EUR", "AFN", "AMD", "LBP", "CNY", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "EUR", "AFN", "AMD", "LBP", "CNY", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number"
+    }
 ]);
 let fromCurrency = currency[userInput.from];
 let toCurrency = currency[userInput.to];
